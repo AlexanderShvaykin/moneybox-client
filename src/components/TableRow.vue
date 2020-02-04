91de94b08c969cb67bd7504c9729b7ffa4212c6e
Alexander Shvaykin <skiline.alex@gmail.com>
2020-02-04 17:02:08 +0300

    tmp

---
+++ src/components/TableRow.vue
@@ -0,0 +1,29 @@
+<template>
+  <tr v-if="columns.length > 0">
+    <td v-for="(value, index) in columns" :key="index">{{ value }}</td>
+    <td>
+      <button
+          @click="$emit('remove')"
+          class="btn btn-outline-danger"
+      >{{ removeText() }}</button>
+    </td>
+  </tr>
+</template>
+
+<script lang="ts">
+  import { Component, Prop, Vue } from 'vue-property-decorator';
+
+  @Component
+  export default class TableRow extends Vue {
+    @Prop() private columns!: [];
+    @Prop() private customRemoveText!: string;
+
+    removeText(): string {
+      return (this.customRemoveText || "remove")
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/MoneyBox.vue
+++ src/pages/MoneyBox.vue
@@ -14,15 +14,17 @@
               <th scope="col">income_amount</th>
               <th scope="col">started_at</th>
               <th scope="col">finished_at</th>
+              <th scope="col"></th>
             </tr>
           </thead>
-          <tbody>
-            <tr v-for="goal of goals" :key="goal.id">
-              <th scope="row">{{ goal.attributes.monthNumber }}</th>
-              <td>{{ goal.attributes.paymentAmount }}</td>
-              <td>{{ goal.attributes.incomeAmount }}</td>
-              <td>{{ goal.attributes.startedAt | timeFormat }}</td>
-              <td>{{ goal.attributes.finishedAt | timeFormat }}</td>
+          <tbody v-if="goals.length > 0">
+            <tr
+                is="TableRow"
+                v-for="(goal, index) in goals"
+                :key="index"
+                :columns="goalColumns(goal)"
+                :remove="goals.splice(index, 1)"
+            >
             </tr>
           </tbody>
         </table>
@@ -52,27 +54,20 @@
   import FinanceGoal from "@/interfaces/financeGoal";
   import DateInput from "@/components/DateInput.vue";
   import ModalForm from "@/components/ModalForm.vue";
+  import TableRow from "@/components/TableRow.vue";
   import Helper from '@/helpers'
 
   @Component({
     components: {
       DateInput,
-      ModalForm
-    },
-    filters: {
-      timeFormat(value: string): string {
-        const result: any = Date.parse(value);
-        if (isNaN(result)) {
-          return  ""
-        } else {
-          return Helper.formatDate(new Date(value))
-        }
-      }
+      ModalForm,
+      TableRow
     }
   })
 
   export default class MoneyBoxPage extends Mixins(Authorized) {
-    private resource!: ResourceMethods;
+    private moneyboxResource!: ResourceMethods;
+    private goalResource!: ResourceMethods;
     private boxId!: string;
     box!: Box;
     goals!: FinanceGoal[];
@@ -81,8 +76,31 @@
     startedAt: string = "";
     finishedAt: string = "";
 
+    goalColumns(goal: FinanceGoal | undefined): any[] {
+      if (goal) {
+        return [
+          goal.attributes.monthNumber,
+          goal.attributes.paymentAmount,
+          goal.attributes.incomeAmount,
+          this.timeFormat(goal.attributes.startedAt),
+          this.timeFormat(goal.attributes.finishedAt)
+        ]
+      } else {
+        return []
+      }
+    }
+
+    timeFormat(value: string): string {
+      const result: any = Date.parse(value);
+      if (isNaN(result)) {
+        return  ""
+      } else {
+        return Helper.formatDate(new Date(value))
+      }
+    }
+
     loadMoneybox(): void {
-      this.resource.get({ id: this.boxId })
+      this.moneyboxResource.get({ id: this.boxId })
         .then(response => {
           response.json().then((resp: { data: Box }) => {
             this.box = resp.data;
@@ -103,6 +121,7 @@
     }
 
     toggleForm(flag: boolean = true): void {
+      console.log(this.goals )
       this.displayForm = flag
     }
 
@@ -110,7 +129,7 @@
       const params = { started_at: this.startedAt, finished_at: this.finishedAt };
 
       // @ts-ignore
-      this.resource.createGoal({id: this.boxId}, params).then((response: Response) => {
+      this.moneyboxResource.createGoal({id: this.boxId}, params).then((response: Response) => {
         response.json().then((resp: { data: FinanceGoal }) => {
           this.goals.push(resp.data);
           this.toggleForm(false)
@@ -119,9 +138,10 @@
     }
 
     registerResource(): void {
-      this.resource = this.$resource("api/moneyboxes{/id}", {}, {
+      this.moneyboxResource = this.$resource("api/moneyboxes{/id}", {}, {
         createGoal: { method: 'POST', url: "api/moneyboxes{/id}/finance_goals"}
       });
+      this.goalResource = this.$resource("api/finance_goals{/id}");
     }
 
     mounted(): void {
